453f7a02fb39f0f4164861036ab435e9b41e8c00
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-18 06:10:14 +0700

    routing dashboard

---
+++ app/dashboard/controller.js
@@ -0,0 +1,12 @@
+
+module.exports={
+  index: async(req, res)=>{
+    try {
+      res.render('index',{
+        title: 'Express Js'
+      })
+    } catch (err) {
+      console.log("err : ", err)
+    }
+  }
+}
