1eb5f8116dc8bdef8c38857d7ce348bb4ce6facc
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-20 00:23:18 +0700

    view update admin profile

--- app/profile/controller.js
+++ app/profile/controller.js
@@ -5,18 +5,38 @@ module.exports = {
     try {
 
       res.render('auth/view_profile', {
-        name: 'admin',
-        title: 'Halaman Dashboard',
-        count: {
-          pending: 1,
-          accepted: 2,
-          rejected: 3,
-          dosenActive: 4
+        title: 'Detail Profile',
+        admin: {
+          full_name: 'Adam Nasrudin',
+          email: 'admin@gmail.com',
+          gender: 'male',
+          role: 'super_admin'
         }
       })
     } catch (err) {
       console.log(err)
 
     }
-  }
+  },
+  
+  viewEdit : async(req, res)=>{
+    try {
+      res.render('auth/edit', {
+        title: 'Ubah Profile',
+        admin: {
+          full_name: 'Adam Nasrudin',
+          email: 'admin@gmail.com',
+          gender: 'male',
+          role: 'super_admin'
+        }
+      })
+      
+    } catch (err) {
+      console.log(err)
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/ajaran')
+    }
+  },
+  
 }
--- app/profile/router.js
+++ app/profile/router.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {index} = require('./controller')
+const {index, viewEdit} = require('./controller')
 
 /* GET home page. */
 router.get('/', index);
+router.get('/edit', viewEdit);
 
 
 module.exports = router;
