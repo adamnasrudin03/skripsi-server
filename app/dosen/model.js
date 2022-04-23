e69fa8343cd59758d1e73b66cb1b6bdf4b36886e
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-23 16:08:55 +0700

    fix model dosen

--- app/dosen/model.js
+++ app/dosen/model.js
@@ -29,76 +29,76 @@ let dosenSchema = mongoose.Schema({
     default: 'Y',
     require: [true, 'Status dosen harus diiisi']
   },
-  jenjang_pendidikan: {
+  pendidikan: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.pendidikan.s2, nilai.pendidikan.s3],
     default: nilai.pendidikan.s2,
     require: [true, 'Jenjang Pendidikan dosen harus diiisi']
   },
-  jabatan_fungsional: {
+  fungsional: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.fungsional.tenagaPengajar, nilai.fungsional.asistenAhli, 
       nilai.fungsional.lektor, nilai.fungsional.lektorKepala],
     default: nilai.fungsional.tenagaPengajar,
     require: [true, 'Jabatan fungsional dosen harus diiisi']
   },
-  bidang_ahli_aplikasi_desktop: {
+  bidang_aplikasi_desktop: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Aplikasi Desktop dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Aplikasi Desktop dosen harus diiisi']
   },
-  bidang_ahli_aplikasi_web: {
+  bidang_aplikasi_web: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Aplikasi Web dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Aplikasi Web dosen harus diiisi']
   },
-  bidang_ahli_aplikasi_mobile: {
+  bidang_aplikasi_mobile: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Aplikasi Mobile dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Aplikasi Mobile dosen harus diiisi']
   },
-  bidang_ahli_data_mining: {
+  bidang_data_mining: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Data Mining dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Data Mining dosen harus diiisi']
   },
-  bidang_ahli_iot: {
+  bidang_iot: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Internet Of Thinks dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Internet Of Thinks dosen harus diiisi']
   },
-  bidang_ahli_desain_aplikasi: {
+  bidang_desain_aplikasi: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Desain Aplikasi dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Desain Aplikasi dosen harus diiisi']
   },
-  bidang_ahli_jaringan_komputer: {
+  bidang_jaringan_komputer: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Jaringan Komputer dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Jaringan Komputer dosen harus diiisi']
   },
-  bidang_ahli_basis_data: {
+  bidang_basis_data: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.bidang.tingkat8, nilai.bidang.tingkat7, nilai.bidang.tingkat6, nilai.bidang.tingkat5, 
       nilai.bidang.tingkat4, nilai.bidang.tingkat3, nilai.bidang.tingkat2, nilai.bidang.tingkat1],
     default: nilai.bidang.tingkat8,
-    require: [true, 'Tingkat keahlian bnidang Basis Data dosen harus diiisi']
+    require: [true, 'Tingkat keahlian bidang Basis Data dosen harus diiisi']
   },
-  jumlah_bimbingan: {
+  jumlah: {
     type: SchemaTypes.Decimal128,
     enum: [nilai.jumlah.sangatBanyak, nilai.jumlah.banyak, 
       nilai.jumlah.cukup, nilai.jumlah.kurang, nilai.jumlah.sangatKurang],
