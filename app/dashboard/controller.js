e34c2bec55a8e0a2e53127684f9b689602c405a5
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-24 23:02:15 +0700

    count dashboard overview

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,18 +1,22 @@
 const Dosen = require('./../dosen/model')
+const Mahasiswa = require('./../mahasiswa/model')
 
 module.exports = {
   index: async (req, res) => {
     try {
 
       const dosen = await Dosen.find({status: "Y"}).count()
+      const accepted = await Mahasiswa.find({status: "accepted"}).count()
+      const rejected = await Mahasiswa.find({status: "rejected"}).count()
+      const pending = await Mahasiswa.find({status: "pending"}).count()
 
       res.render('admin/dashboard/view_dashboard', {
         name: 'admin',
         title: 'Halaman Dashboard',
         count: {
-          pending: 1,
-          accepted: 2,
-          rejected: 3,
+          pending: pending,
+          accepted: accepted,
+          rejected: rejected,
           dosenActive: dosen
         }
       })
