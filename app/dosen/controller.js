aadcb5a4687c4e3f83ea4b4e1d1dcb695fadf41d
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-24 12:15:52 +0700

    action create & edit status

--- app/dosen/controller.js
+++ app/dosen/controller.js
@@ -20,5 +20,68 @@ module.exports={
       res.redirect('/dosen')
     }
   },
+  
+  viewCreate: async(req, res)=>{
+    try {
+      res.render('admin/dosen/create',{
+        title: 'Taambah Dosen Pembimbing'
+      })
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/dosen')
+    }
+  },
+
+  actionCreate : async(req, res)=>{
+    try {
+      const { nidn, nama, gelar, email, no_wa, 
+        pendidikan, fungsional, jumlah, bidang_aplikasi_desktop,
+        bidang_aplikasi_web, bidang_aplikasi_mobile, bidang_data_mining,
+        bidang_iot, bidang_desain_aplikasi, bidang_jaringan_komputer, 
+        bidang_basis_data } = req.body
+
+      let dosen = await Dosen({ nidn, nama, gelar, email, no_wa, 
+        pendidikan, fungsional, jumlah, bidang_aplikasi_desktop,
+        bidang_aplikasi_web, bidang_aplikasi_mobile, bidang_data_mining,
+        bidang_iot, bidang_desain_aplikasi, bidang_jaringan_komputer, 
+        bidang_basis_data })
+      await dosen.save();
+
+      req.flash('alertMessage', "Berhasil tambah data tdosen pembimbing")
+      req.flash('alertStatus', "success")
+
+      res.redirect('/dosen')
+      
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/dosen')
+    }
+  },
+
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params
+      let dosen = await Dosen.findOne({ _id: id })
+
+      let status = dosen.status === 'Y' ? 'N' : 'Y'
+
+      await Dosen.findOneAndUpdate({
+        _id: id
+      }, { status })
+
+      req.flash('alertMessage', "Berhasil ubah status")
+      req.flash('alertStatus', "success")
+
+      res.redirect('/dosen')
+
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/dosen')
+    }
+  }
 
 }
