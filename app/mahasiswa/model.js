fa97245171624978555f2914dfb23f35e0facae5
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-24 15:42:20 +0700

    model schema mahasiswa

---
+++ app/mahasiswa/model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+
+let mahasiswaSchema = mongoose.Schema({
+  npm: {
+    type: String,
+    require: [true, 'Nomor Pokok Mahasiswa harus diisi']
+  },
+  nama: {
+    type: String,
+    require: [true, 'Nama mahasiswa harus diiisi'],
+    maxlength :[225, "panjang nama harus antara 3 - 225 karakter"],
+    minlength :[3, "panjang nama harus antara 3 - 225 karakter"]
+  },
+  email: {
+    type: String,
+    require: [true, 'Email mahasiswa harus diiisi']
+  },
+  semester: {
+    type: String,
+    require: [true, 'Semester mahasiswa saat ini harus diiisi']
+  },
+  no_wa: {
+    type: String,
+    require: [true, 'No Whatsapp mahasiswa harus diiisi'],
+    maxlength :[13, "panjang nomor whatsapp harus antara 9 - 13 karakter"],
+    minlength :[9, "panjang nomor whatsapp harus antara 9 - 13 karakter"]
+  },
+  judul_skripsi: {
+    type: String,
+    require: [true, 'Judul skripsi yang akan dibuat mahasiswa harus diiisi'],
+    maxlength :[225, "panjang judul skripsi harus antara 3 - 225 karakter"],
+    minlength :[3, "panjang judul skripsi harus antara 3 - 225 karakter"]
+  },
+  tema_skripsi: {
+    type: String,
+    enum: ['bidang_aplikasi_desktop', 'bidang_aplikasi_web', 'bidang_aplikasi_mobile', 
+      'bidang_data_mining', 'bidang_iot', 'bidang_desain_aplikasi', 
+      'bidang_jaringan_komputer', 'bidang_basis_data'],
+    require: [true, 'Tema skripsi yang akan dibuat mahasiswa harus diiisi']
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending',
+    require: [true, 'Status pengajuan proposal skripsi harus diiisi']
+  },
+  file_proposal : {type : String},
+  file_rekap_nilai : {type : String},
+  mata_kuliah_lain : {type : String, default: '-'},
+  dosen_sebelum : {type : String, default: '-'},
+  ajaran_id : {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ajaran'
+  },
+  dosen_id : {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Dosen'
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Mahasiswa', mahasiswaSchema)
