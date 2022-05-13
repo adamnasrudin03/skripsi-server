e35b3a7cb1415c804a85d4f5d69d8dca63b4d813
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-05-13 16:16:05 +0700

    action edit profile

--- app/profile/controller.js
+++ app/profile/controller.js
@@ -1,17 +1,22 @@
-
+const User = require('./../users/model')
 
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash("alertMessage")
+      const alertStatus = req.flash("alertStatus")
+      const alert = { message: alertMessage, status: alertStatus}
 
       res.render('auth/view_profile', {
         title: 'Detail Profile',
+        alert,
         name: req.session.user.name,
         admin: req.session.user
       })
     } catch (err) {
-      console.log(err)
-
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/profile')
     }
   },
   
@@ -27,7 +32,114 @@ module.exports = {
       console.log(err)
       req.flash('alertMessage', `${err.message}`)
       req.flash('alertStatus', 'danger')
-      res.redirect('/ajaran')
+      res.redirect('/profile')
+    }
+  },
+
+  actionEdit : async(req, res)=>{
+    try {
+      const { id } = req.params;
+      
+      const checkById = await User.findOne({ _id: id });
+      if (!checkById) {
+        req.flash('alertMessage', `Data tidak terdaftar`)
+        req.flash('alertStatus', 'danger')
+        res.redirect('/profile')
+        return;
+      }
+
+      const { email, name, gender, phoneNumber} = req.body;
+
+      const checkByEmail = await User.findOne({ email: email })
+      
+      if (checkByEmail && (checkByEmail._id.toString() !== checkById._id.toString())) {
+        req.flash('alertMessage', `Email yang anda inputkan sudah terdaftar`)
+        req.flash('alertStatus', 'danger')
+        res.redirect('/profile')
+        return;
+      }
+
+      if(req.image){
+        let tmp_path= req.file.path;
+        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+        let filename = req.file.filename + '.' + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+
+        const src = fs.createReadStream(tmp_path)
+        const dest = fs.createWriteStream(target_path)
+
+        src.pipe(dest)
+
+        src.on('end', async ()=>{
+          try {
+            const check = await User.findOne({_id: id})
+
+            let currentImage = `${config.rootPath}/public/uploads/${check.thumbnial}`;
+            if(fs.existsSync(currentImage)){
+              fs.unlinkSync(currentImage)
+            }
+
+            await User.findOneAndUpdate({
+              _id: id
+            },{ 
+              email, 
+              gender, 
+              name, 
+              phoneNumber,
+              thumbnial: filename
+             })
+             
+            const user = await User.findOne({_id: id})
+
+            req.session.user = {
+              id: user._id,
+              email: user.email,
+              status: user.status,
+              name: user.name,
+              gender: user.gender,
+              role: user.role,
+              phoneNumber: user.phoneNumber,
+              thumbnial: user.thumbnial
+            }
+      
+            req.flash('alertMessage', "Berhasil ubah data profile")
+            req.flash('alertStatus', "success")
+      
+            res.redirect('/profile')
+            
+          } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/profile')
+          }
+        })
+      } else {
+        await User.findOneAndUpdate({
+          _id: id
+        },{ email, gender, name, phoneNumber })
+  
+        const user = await User.findOne({_id: id})
+
+        req.session.user = {
+          id: user._id,
+          email: user.email,
+          status: user.status,
+          name: user.name,
+          gender: user.gender,
+          role: user.role,
+          phoneNumber: user.phoneNumber,
+          thumbnial: user.thumbnial
+        }
+        
+        req.flash('alertMessage', "Berhasil ubah data profile")
+        req.flash('alertStatus', "success")
+  
+        res.redirect('/profile')
+      }
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/profile')
     }
   },
 
@@ -39,7 +151,9 @@ module.exports = {
         title: 'Ganti Kata Sandi',
       })
     } catch (err) {
-      console.log(err)
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/profile')
 
     }
   },
--- app/profile/router.js
+++ app/profile/router.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewEdit, viewChangePassword} = require('./controller')
+const {index, viewEdit, viewChangePassword, actionEdit} = require('./controller');
+const multer = require('multer');
+const os = require('os');
 
+const { isLoginAdmin } = require('../middleware/auth')
+
+router.use(isLoginAdmin);
 router.get('/', index);
 router.get('/edit', viewEdit);
+router.put('/edit/:id', 
+    multer({ dest: os.tmpdir() }).single('image'),
+    actionEdit
+);
 router.get('/change-password', viewChangePassword);
 
 
